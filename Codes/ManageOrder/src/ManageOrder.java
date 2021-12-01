1dd08a724751c23bffebed4673abf44ee07ff68f
Sohail Sadiq <SP20-BSE-073@cuiatd.edu.pk>
2021-12-01 18:02:29 +0500

    Manage Order
    User Interface For Manage Orders

---
+++ Codes/ManageOrder/src/ManageOrder.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SP20-BSE-047
+ */
+public class ManageOrder {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
